c7d3bbe89bb331f9ddb974a5fc12a9bd9ff488c6
Matt Sturgeon <matt@sturgeon.me.uk>
2016-09-28 15:02:25 +0100

    Adds a method "isPlayerAFK(Player)" that checks if the given player is currently AFK.
    For now this method checks with the WhosAFK plugin, but it has been
    written to allow checking with multiple plugins in the future.

--- src/main/java/de/themoep/BetterBeds/BetterBeds.java
+++ src/main/java/de/themoep/BetterBeds/BetterBeds.java
@@ -1,5 +1,6 @@
 package de.themoep.BetterBeds;
 
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.logging.Level;
 
@@ -144,7 +145,7 @@ public class BetterBeds extends JavaPlugin implements Listener {
             if(world.equals(p.getWorld())) {
                 if (!p.isSleeping() && p.hasPermission("betterbeds.ghost") && !p.hasPermission("betterbeds.ghost.buster"))
                     return false;
-                if (p.hasPermission("betterbeds.sleep") && !p.hasPermission("betterbeds.ignore"))
+                if (p.hasPermission("betterbeds.sleep") && !p.hasPermission("betterbeds.ignore") && !isPlayerAFK(p))
                     calculatedPlayers++;
             }
 		}		
@@ -306,9 +307,43 @@ public class BetterBeds extends JavaPlugin implements Listener {
 	private int countQualifyingPlayers(World world) {
 		int calculatedPlayers = 0;
 		for(Player p : getServer().getOnlinePlayers())
-			if(world.equals(p.getWorld()) && !p.hasPermission("betterbeds.ignore") && p.hasPermission("betterbeds.sleep"))
+			if(world.equals(p.getWorld()) && !p.hasPermission("betterbeds.ignore") && p.hasPermission("betterbeds.sleep") && !isPlayerAFK(p))
 				calculatedPlayers ++;
 		return calculatedPlayers;
+	}
+
+	/**
+	 * Check if a player is AFK
+	 * Currently this only works with the WhosAFK plugin
+	 * TODO: Add support for checking with more methods
+	 * TODO: Add a config option to decide whether AFK players should be counted
+	 * @param Player
+	 * @return boolean - True if Player is currently AFK
+	 */
+	private boolean isPlayerAFK(Player p) {
+		ClassLoader classLoader = BetterBeds.class.getClassLoader();
+
+		// Check if the player is AFK, according to WhosAFK
+		try {
+			// Load the WhosAFK class and it's playerIsAFK(Player) method
+			Class<?> WhosAFK = classLoader.loadClass("whosafk.WhosAFK");
+			Method whosafkPlayerIsAFK = WhosAFK.getMethod("playerIsAFK", Player.class);
+
+			// Get the instance of WhosAFK being used by spigot
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			JavaPlugin whosafk = JavaPlugin.getPlugin((Class) WhosAFK);
+
+			// Finally, check if WhosAFK thinks the player is AFK
+			if (whosafk.isEnabled() && (boolean) whosafkPlayerIsAFK.invoke(whosafk, p))
+				return true;
+		} catch (ClassNotFoundException e) {
+			// WhosAFK is not installed, no need to panic
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Default to not AFK
+		return false;
 	}
 
 	/**
